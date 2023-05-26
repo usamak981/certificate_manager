cfc235658ccd8109aff546b75fc00dad18598e8a
mars-96 <mars73348@gmail.com>
2023-05-26 23:10:00 +0500

    test

--- accounts/views.py
+++ accounts/views.py
@@ -20,7 +20,9 @@ def error_page(request):
 def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
+        
         password = request.POST.get('password')
+        
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request)
